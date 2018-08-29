69bec2a127c916ea366777f04a2dbd595a22578a
Kamil Shakirov <35695513+mrazots@users.noreply.github.com>
2018-08-29 16:33:19 +0300

    Update ex3.c

--- Week2/ex3.c
+++ Week2/ex3.c
@@ -1,15 +1,20 @@
-#include<stdio.h> 
 
-int main() { 
-int len; 
-scanf("%d", &len); 
-for (int i = 0; i < len; i++) { 
-    for (int j = 0; j < len - i; j++) { 
-        printf(" "); 
-    } 
-    for (int j = 0; j < 2 * i - 1; j++) { 
-        printf("*"); 
-    } 
-    printf("\n"); 
-    } 
+#include <stdio.h>
+#include <string.h>
+ 
+int main()
+{
+  	char str[50], strrev[50];
+  	int i, j, len;
+    scanf("%s", str); 
+    len = strlen(str);
+  	j = 0;
+  	for (i = len - 1; i >= 0; i--)
+  	{
+  		strrev[j++] = str[i];
+  	}
+
+  	printf("\n%s", strrev);
+  
+  	return 0;
 }
