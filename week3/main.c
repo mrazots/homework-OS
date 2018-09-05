f6b7d61032927f84addbefd7d89465a05d913010
Kamil Shakirov <35695513+mrazots@users.noreply.github.com>
2018-09-05 19:34:56 +0300

    Add files via upload

---
+++ week3/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void swap(int *a, int *b)
+{
+    int c = *a;
+    *a = *b;
+    *b = c;
+}
+
+void bubble_sort(int *arr, int n)
+{
+    int i, j;
+    for (i = 0; i < n-1; i++){
+        for (j = 0; j < n-i-1; j++){
+            if (arr[j] > arr[j+1])
+                swap(&arr[j], &arr[j+1]);
+        }
+
+    }
+
+}
+
+int main()
+{
+    int s;
+    printf("Write number of elements: ");
+    scanf("%d",&s);
+    int a[s];
+    int i;
+    for (i = 0; i<s; i++){
+        scanf("%d", &a[i]);
+    }
+    bubble_sort(a, s);
+
+    for (i=0; i < s; i++)
+        printf("%d ", a[i]);
+    return 0;
+}
